9a8beb33af4a3a545af7917818b08f115dc41b96
LauraRamirezCampus <lauravramirez0327@gmail.com>
2023-05-29 17:14:29 -0500

    feat: :sparkles: 7.se declara variables
    Se declara variable y las diferentes maneras de imprimir

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-  
+  /**
+   * ?imprime en formato html
+   */
+  echo '<h1>Hello world</h1>';
+
+  /**declara la variable iniciando con $ */
+  $texto= "Mundo";
+
+  /**imprime la variable con printf */
+  printf("Hola %s", $texto);
+
+  /**
+   * ?sprintf devuelve la cadena
+   */
+  $mensaje= sprintf("Hola %s", $texto);
+  echo $mensaje;
+
+
 ?>
